7cef74a21309a0cfc777fcc68611c50f1c135afc
Lady of Darkness <85314429+LadyDarknes@users.noreply.github.com>
2025-03-15 04:01:06 +0300

    Create Sc.lua

---
+++ Sc.lua
@@ -0,0 +1,46 @@
+local UIS = game:GetService("UserInputService")
+local VIM = game:GetService("VirtualInputManager")
+local runService = game:GetService("RunService")
+local isHolding = false
+
+local jumpSpeed = 0.09  -- Minimum 90ms
+local waitTime = 0.5  -- Bekleme süresi 0.5s olarak ayarlandı
+
+-- Alt tuşuna basılı tutma ve zıplama
+UIS.InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.LeftAlt and not isHolding then
+        isHolding = true
+        local lastJumpTime = 0
+        while isHolding do
+            -- Zıplama için koşul: Her 90ms'de bir
+            local currentTime = tick()
+            if currentTime - lastJumpTime >= jumpSpeed then
+                -- 2 kez zıpla
+                for i = 1, 2 do
+                    VIM:SendKeyEvent(true, Enum.KeyCode.Space, false, game)
+                    VIM:SendKeyEvent(false, Enum.KeyCode.Space, false, game)
+                    lastJumpTime = currentTime
+                    task.wait(0.1)  -- Kısa bekleme
+                end
+                task.wait(waitTime)  -- Bekleme süresi
+            end
+            runService.Heartbeat:Wait()  -- Daha hassas zamanlama için Heartbeat kullanıyoruz
+        end
+    end
+end)
+
+UIS.InputEnded:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.LeftAlt then
+        isHolding = false
+    end
+end)
+
+local plr = game:GetService"Players".LocalPlayer
+local m = plr:GetMouse()
+m.KeyDown:connect(function(k)
+	if k == "v" then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+		wait()
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Seated")
+	end
+end)
